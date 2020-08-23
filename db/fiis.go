0bb01005fa4ccb4b0136ffd525c7b44b9b1e1785
Marcelo Jorge Vieira <metal@alucinados.com>
2020-08-22 23:17:45 -0300

    Merging fii and fii_operations

--- db/fiis.go
+++ db/fiis.go
@@ -7,11 +7,23 @@ import (
 	"github.com/mfinancecombr/finance-wallet-api/wallet"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (m *mongoSession) getAllFIIs(c string) (wallet.FIIList, error) {
+func (m *mongoSession) InsertFIIOperation(d *wallet.FII) (*mongo.InsertOneResult, error) {
+	log.Debug("[DB] InsertFIIOperation")
+	return m.insertOperation(d)
+}
+
+func (m *mongoSession) UpdateFIIOperationByID(id string, d *wallet.FII) (*mongo.UpdateResult, error) {
+	log.Debug("[DB] UpdateFIIOperationByID")
+	d.ID = ""
+	return m.updateOperation(operationsCollection, id, d)
+}
+
+func (m *mongoSession) GetAllFIIsOperations() (wallet.FIIList, error) {
 	log.Debug("[DB] GetAllFIIsOperations")
-	results, err := m.collection.FindAll(c, bson.M{})
+	results, err := m.collection.FindAll(operationsCollection, bson.M{})
 	if err != nil {
 		return nil, err
 	}
@@ -23,4 +35,16 @@ func (m *mongoSession) getAllFIIs(c string) (wallet.FIIList, error) {
 		operationsList = append(operationsList, buy)
 	}
 	return operationsList, nil
+}
+
+func (m *mongoSession) GetFIIOperationByID(id string) (*wallet.FII, error) {
+	log.Debug("[DB] GetFIIOperationByID")
+	fii := &wallet.FII{}
+	if err := m.getOperationByID(operationsCollection, id, fii); err != nil {
+		return nil, err
+	}
+	if fii.Symbol == "" {
+		return nil, nil
+	}
+	return fii, nil
 }
