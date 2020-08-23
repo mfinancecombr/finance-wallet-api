72314626f17bf305cd487da16caf4894258b21c9
Marcelo Jorge Vieira <metal@alucinados.com>
2020-08-22 23:32:56 -0300

    Merging certificates_of_deposit and certificates_of_deposit_operations

--- db/certificates_of_deposit.go
+++ db/certificates_of_deposit.go
@@ -7,11 +7,23 @@ import (
 	"github.com/mfinancecombr/finance-wallet-api/wallet"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (m *mongoSession) getAllCertificatesOfDeposits(c string) (wallet.CertificateOfDepositList, error) {
-	log.Debug("[DB] getAllCertificatesOfDeposits")
-	results, err := m.collection.FindAll(c, bson.M{})
+func (m *mongoSession) InsertCertificateOfDepositOperation(d *wallet.CertificateOfDeposit) (*mongo.InsertOneResult, error) {
+	log.Debug("[DB] InsertCertificateOfDepositOperation")
+	return m.insertOperation(d)
+}
+
+func (m *mongoSession) UpdateCertificateOfDepositOperationByID(id string, d *wallet.CertificateOfDeposit) (*mongo.UpdateResult, error) {
+	log.Debug("[DB] UpdateCertificateOfDepositOperationByID")
+	d.ID = ""
+	return m.updateOperation(operationsCollection, id, d)
+}
+
+func (m *mongoSession) GetAllCertificatesOfDepositsOperations() (wallet.CertificateOfDepositList, error) {
+	log.Debug("[DB] GetAllCertificatesOfDepositsOperations")
+	results, err := m.collection.FindAll(operationsCollection, bson.M{})
 	if err != nil {
 		return nil, err
 	}
@@ -23,4 +35,16 @@ func (m *mongoSession) getAllCertificatesOfDeposits(c string) (wallet.Certificat
 		operationsList = append(operationsList, buy)
 	}
 	return operationsList, nil
+}
+
+func (m *mongoSession) GetCertificateOfDepositOperationByID(id string) (*wallet.CertificateOfDeposit, error) {
+	log.Debug("[DB] GetCertificateOfDepositOperationByID")
+	certificateOfDeposit := &wallet.CertificateOfDeposit{}
+	if err := m.getOperationByID(operationsCollection, id, certificateOfDeposit); err != nil {
+		return nil, err
+	}
+	if certificateOfDeposit.Symbol == "" {
+		return nil, nil
+	}
+	return certificateOfDeposit, nil
 }
