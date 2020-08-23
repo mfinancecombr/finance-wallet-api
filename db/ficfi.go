156b96d3b569e4c3555235c82f3eda12a2ea036d
Marcelo Jorge Vieira <metal@alucinados.com>
2020-08-22 23:22:20 -0300

    Merging ficfi and ficfi_operations

--- db/ficfi.go
+++ db/ficfi.go
@@ -7,11 +7,23 @@ import (
 	"github.com/mfinancecombr/finance-wallet-api/wallet"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (m *mongoSession) getAllFICFI(c string) (wallet.FICFIList, error) {
-	log.Debug("[DB] getAllFICFI")
-	results, err := m.collection.FindAll(c, bson.M{})
+func (m *mongoSession) InsertFICFIOperation(d *wallet.FICFI) (*mongo.InsertOneResult, error) {
+	log.Debug("[DB] InsertFICFIOperation")
+	return m.insertOperation(d)
+}
+
+func (m *mongoSession) UpdateFICFIOperationByID(id string, d *wallet.FICFI) (*mongo.UpdateResult, error) {
+	log.Debug("[DB] UpdateFICFIOperationByID")
+	d.ID = ""
+	return m.updateOperation(operationsCollection, id, d)
+}
+
+func (m *mongoSession) GetAllFICFIOperations() (wallet.FICFIList, error) {
+	log.Debug("[DB] GetAllFICFIOperations")
+	results, err := m.collection.FindAll(operationsCollection, bson.M{})
 	if err != nil {
 		return nil, err
 	}
@@ -23,4 +35,16 @@ func (m *mongoSession) getAllFICFI(c string) (wallet.FICFIList, error) {
 		operationsList = append(operationsList, buy)
 	}
 	return operationsList, nil
+}
+
+func (m *mongoSession) GetFICFIOperationByID(id string) (*wallet.FICFI, error) {
+	log.Debug("[DB] GetFICFIOperationByID")
+	ficfi := &wallet.FICFI{}
+	if err := m.getOperationByID(operationsCollection, id, ficfi); err != nil {
+		return nil, err
+	}
+	if ficfi.Symbol == "" {
+		return nil, nil
+	}
+	return ficfi, nil
 }
