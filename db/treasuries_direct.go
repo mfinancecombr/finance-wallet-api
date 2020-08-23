335d467a3e077318380a77600588003923184278
Marcelo Jorge Vieira <metal@alucinados.com>
2020-08-22 23:27:48 -0300

    Merging treasuries_direct and treasuries_direct_operations

--- db/treasuries_direct.go
+++ db/treasuries_direct.go
@@ -7,10 +7,22 @@ import (
 	"github.com/mfinancecombr/finance-wallet-api/wallet"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func (m *mongoSession) getAllTreasuriesDirects(c string) (wallet.TreasuryDirectList, error) {
-	log.Debug("[DB] getAllTreasuriesDirects")
+func (m *mongoSession) InsertTreasuryDirectOperation(d *wallet.TreasuryDirect) (*mongo.InsertOneResult, error) {
+	log.Debug("[DB] InsertTreasuryDirectOperation")
+	return m.insertOperation(d)
+}
+
+func (m *mongoSession) UpdateTreasuryDirectOperationByID(id string, d *wallet.TreasuryDirect) (*mongo.UpdateResult, error) {
+	log.Debug("[DB] UpdateTreasuryDirectOperationByID")
+	d.ID = ""
+	return m.updateOperation(operationsCollection, id, d)
+}
+
+func (m *mongoSession) GetAllTreasuriesDirectsOperations() (wallet.TreasuryDirectList, error) {
+	log.Debug("[DB] GetAllTreasuriesDirectsOperations")
 	results, err := m.collection.FindAll(operationsCollection, bson.M{})
 	if err != nil {
 		return nil, err
@@ -23,4 +35,16 @@ func (m *mongoSession) getAllTreasuriesDirects(c string) (wallet.TreasuryDirectL
 		treasuryDirectList = append(treasuryDirectList, treasuryDirect)
 	}
 	return treasuryDirectList, nil
+}
+
+func (m *mongoSession) GetTreasuryDirectOperationByID(id string) (*wallet.TreasuryDirect, error) {
+	log.Debug("[DB] GetTreasuryDirectOperationByID")
+	treasuryDirect := &wallet.TreasuryDirect{}
+	if err := m.getOperationByID(operationsCollection, id, treasuryDirect); err != nil {
+		return nil, err
+	}
+	if treasuryDirect.Symbol == "" {
+		return nil, nil
+	}
+	return treasuryDirect, nil
 }
